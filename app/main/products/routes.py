95992c4e71a5470ef71a2f31eb443ac0a671acc1
Tarek Meftah <sudtechnics@gmail.com>
2019-03-02 21:00:12 +0100

    add same fake invoices

--- app/main/products/routes.py
+++ app/main/products/routes.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required
 from ..resources import api
@@ -10,7 +11,35 @@ class List(Resource):
     @requires_access_level(ACCESS['guest'])
     def get(self):
         return {
-            'message': ' index page'
+            'invoices': [
+               {
+                   'id': 0,
+                   'name': "INV0001",
+                   'client': "Client-1",
+                   'amount': "159.52",
+                   'currency': "$",
+                   'term': "2 Days",
+                   'due': "01-08-2019"
+               },
+                {
+                   'id': 1,
+                   'name': "INV0002",
+                   'client': "Client-2",
+                   'amount': "200.00",
+                   'currency': "€",
+                   'term': "Due on Recept",
+                   'due': "17-07-2019"
+               },
+                {
+                   'id': 2,
+                   'name': "INV0003",
+                   'client': "Client-3",
+                   'amount': "202.00",
+                   'currency': "€",
+                   'term': "Due on Recept",
+                   'due': "17-07-2019"
+               }
+            ]
         }
 
 
